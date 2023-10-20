aba8ca7fef9850417970354ea11276e9eabf5e7c
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-10-20 11:03:30 +0300

    create hero-header component

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,64 @@
+import {
+  Button,
+  Container,
+  SimpleGrid,
+  Wrap,
+  WrapItem,
+} from '@chakra-ui/react';
+import React from 'react';
+import heroImage from '../../public/images/background/hero-image.svg';
+import heroBG from '../../public/images/background/hero-bg.png';
+import Image from 'next/image';
+import Blur from '@/SVG/Blur';
+
+function Header() {
+  return (
+    <header
+      className='header'
+      style={{ backgroundImage: `url('${heroBG.src}')` }}
+    >
+      <div className='shapes'>
+        <Blur className='blur' />
+      </div>
+
+      <Container>
+        <SimpleGrid
+          columns={{ md: 2 }}
+          spacing={10}
+          justifyContent='space-between'
+          alignItems='center'
+          height='100vh'
+        >
+          <div className='info'>
+            <div className='title'>
+              NFT Template for Marketplace and Web3 Platforms
+            </div>
+            <div className='subtitle'>
+              Template for NFT, Token, and Web3 marketplace projects, based on
+              Tailwind CSS. Comes with all the essential UI components and pages
+              you need to build an NFT marketplace or all sorts of Web3
+              platforms.
+            </div>
+
+            <Wrap spacing='30px'>
+              <WrapItem>
+                <Button size='lg'>Explore now</Button>
+              </WrapItem>
+              <WrapItem>
+                <Button size='lg' variant='outline'>
+                  Upload Your Art
+                </Button>
+              </WrapItem>
+            </Wrap>
+          </div>
+
+          <div className='image'>
+            <Image src={heroImage} width={470} height={450} alt='hero' />
+          </div>
+        </SimpleGrid>
+      </Container>
+    </header>
+  );
+}
+
+export default Header;
