f02f8e0b554e488ed45b052c40bc0d981a29eba9
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-10-20 10:52:11 +0300

    create millify formatter helper

---
+++ src/helpers/millify.ts
@@ -0,0 +1,27 @@
+import millify from 'millify';
+
+/**
+ * Formats a given number with a precision of 2 decimal places.
+ *
+ * @param number - The number to be formatted.
+ * @returns The formatted number with a precision of 2 decimal places.
+ */
+function formateNumber(number: number) {
+  return millify(number, { precision: 2, locales: 'en' });
+}
+
+/**
+ * Formats a given number into a human-readable string representation with appropriate units (B, KB, MB, GB, TB) and precision (up to 2 decimal places).
+ *
+ * @param number - The number to be formatted.
+ * @returns A human-readable string representation of the input number with appropriate units and precision.
+ */
+function formateUnit(number: number) {
+  return millify(number, {
+    precision: 2,
+    units: ['B', 'KB', 'MB', 'GB', 'TB'],
+    locales: 'en',
+  });
+}
+
+export { formateNumber, formateUnit };
