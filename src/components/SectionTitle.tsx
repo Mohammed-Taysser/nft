4056bdf337c5b95441478e7869f3faf58ef29941
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-10-20 10:48:18 +0300

    create section-title component

---
+++ src/components/SectionTitle.tsx
@@ -0,0 +1,26 @@
+import { Button } from '@chakra-ui/react';
+import Link from 'next/link';
+import { Children } from 'react';
+
+function SectionTitle(props: SectionTitleProps) {
+  const { title, url } = props;
+  return (
+    <div className='section-title'>
+      <div className='wrapper'>
+        <div className='title'>{title}</div>
+
+        <div className=''>
+          {Children.count(props.children) > 0
+            ? props.children
+            : url && (
+                <Button as={Link} href={url}>
+                  View All
+                </Button>
+              )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SectionTitle;
