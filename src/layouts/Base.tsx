7e38d5b2fe4fc1b4aeec63f1c0a54fe5c2bfd3dc
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-10-20 11:03:52 +0300

    create base layout

---
+++ src/layouts/Base.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Chakra from '@/providers/chakra';
+import Footer from './Footer';
+import Navbar from './Navbar';
+import { ColorModeScript } from '@chakra-ui/react';
+
+function Base(props: { children: React.ReactNode }) {
+  return (
+    <>
+      <ColorModeScript initialColorMode='dark' />
+      <Chakra>
+        <Navbar />
+        {props.children}
+        <Footer />
+      </Chakra>
+    </>
+  );
+}
+
+export default Base;
