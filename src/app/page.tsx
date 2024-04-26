4cfd9368e4f089aee61c963c3ce3c387ef37075c
mohammed-taysser <mohamedtaysser983@gmail.com>
2024-04-26 17:37:59 +0300

    update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,62 @@
-import HomepageContent from '@/content/Homepage.content';
-import SEO from '@/helpers/seo';
+import { ARTISTS, AUCTIONS, PRODUCTS } from '@/MOCK';
+import SectionTitle from '@/components/SectionTitle';
+import Artists from '@/components/artist/Artists';
+import Auctions from '@/components/auction/Auctions';
+import Collections from '@/components/collections/Collections';
+import Features from '@/components/feature/Features';
+import Products from '@/components/products/Products';
+import Header from '@/layouts/Header';
+import { Box, Container, Wrap, WrapItem } from '@chakra-ui/react';
+import { Metadata } from 'next';
+import { FaArrowLeftLong, FaArrowRightLong } from 'react-icons/fa6';
 
-export const metadata = {
-  ...SEO.homepage,
+export const metadata: Metadata = {
+  title: 'Homepage',
 };
 
 function Homepage() {
-  return <HomepageContent />;
+  return (
+    <>
+      <Header />
+
+      <Container>
+        <SectionTitle title='Artists'>
+          <Wrap>
+            <WrapItem>
+              <button className='artist-btn artist-prev-btn' title='prev'>
+                <FaArrowLeftLong />
+              </button>
+            </WrapItem>
+            <WrapItem>
+              <button className='artist-btn artist-next-btn' title='next'>
+                <FaArrowRightLong />
+              </button>
+            </WrapItem>
+          </Wrap>
+        </SectionTitle>
+
+        <Artists artists={ARTISTS} />
+
+        <Box my={70}></Box>
+
+        <SectionTitle title='Live Auctions' url='/auctions' />
+
+        <Auctions auctions={AUCTIONS.slice(0, 3)} />
+
+        <Box my={70}></Box>
+
+        <SectionTitle title="Today's Picks" url='/products' />
+
+        <Products products={PRODUCTS.slice(0, 6)} />
+
+        <Box my={70}></Box>
+
+        <Features />
+
+        <Collections />
+      </Container>
+    </>
+  );
 }
 
 export default Homepage;
